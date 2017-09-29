915d5af1c8edc3eb64f03e4f5fedf532c8f98ba7
arthur <chang>
2017-09-29 23:03:24 +0800

    Backend: Setup Autofac

---
+++ WhereWeGoAPI/WhereWeGo/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WhereWeGoAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            builder.RegisterWebApiModelBinderProvider();
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
